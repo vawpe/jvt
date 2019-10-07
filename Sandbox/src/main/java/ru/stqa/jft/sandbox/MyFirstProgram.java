359f5fcfa1cd724e1e44d026be1d040627d65861
Ed.Changli <eduard.changli@mail.ru>
2019-10-08 00:20:13 +0300

    Update MyFirstProgram.java
    ренэйм

--- Sandbox/src/main/java/ru/stqa/jft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/jft/sandbox/MyFirstProgram.java
@@ -1,6 +1,6 @@
 package ru.stqa.jft.sandbox;
 
-public class MyFirstProgramm {
+public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
