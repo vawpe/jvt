b8573fcf2416ed3d1b3e9a360330e7d9ee0b7807
Ed.Changli <eduard.changli@mail.ru>
2019-10-12 20:37:52 +0300

    знакомство с функциями

--- Sandbox/src/main/java/ru/stqa/jft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/jft/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package ru.stqa.jft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("World");
+        hello("Ed");
+        hello("Sasha");
+
+        double l = 6;
+        double a = 5;
+        double b = 10;
+
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
     }
 
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
