881fa6049ab7309b78dd6fe64ee2ccb9a1bb363a
Ed.Changli <eduard.changli@mail.ru>
2019-10-22 00:17:12 +0300

    x

--- Sandbox/src/main/java/ru/stqa/jft/sandbox/Equation.java
+++ Sandbox/src/main/java/ru/stqa/jft/sandbox/Equation.java
@@ -16,15 +16,26 @@ public class Equation {
 
         double d = b * b - 4 * a * c;
 
-        if (d > 0) {
-            n = 2;
-        } else {
-            if (d == 0) {
-                n = 1;
-            } else {
+        if (a != 0) {
+            if (d > 0) {
+                n = 2;
+
+            } else if (d != 0) {
                 n = 0;
+
+            } else {
+                n = 1;
+
             }
-        }
+        } else if (b != 0) {
+                n = 1;
+
+        } else if (c != 0) {
+                n = 0;
+
+        } else {
+                n = -1;
+          }
     }
 
     public int rootNumber() {
--- Sandbox/src/test/java/ru/stqa/jft/sandbox/EquationTests.java
+++ Sandbox/src/test/java/ru/stqa/jft/sandbox/EquationTests.java
@@ -22,4 +22,22 @@ public class EquationTests {
         Equation e = new Equation(1,5,6);
         Assert.assertEquals(2, e.rootNumber(), 0.0);
     }
+
+    @Test
+    public void testLinear() {
+        Equation e = new Equation(0,1,1);
+        Assert.assertEquals(1, e.rootNumber(), 0.0);
+    }
+
+    @Test
+    public void testConstant() {
+        Equation e = new Equation(0,0,1);
+        Assert.assertEquals(0, e.rootNumber(), 0.0);
+    }
+
+    @Test
+    public void testZero() {
+        Equation e = new Equation(0,0,0);
+        Assert.assertEquals(-1, e.rootNumber(), 0.0);
+    }
 }
