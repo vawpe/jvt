c84e3f372d0b224c63080fcba2cc57ba47478af1
Ed.Changli <eduard.changli@mail.ru>
2019-10-22 01:06:26 +0300

    123

--- addressbook-web-test/src/test/java/ru/stqa/jft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/jft/addressbook/appmanager/HelperBase.java
@@ -12,8 +12,11 @@ public class HelperBase {
 
     public void type(By locator, String text) {
         if (text != null) {
-            driver.findElement(locator).clear();
-            driver.findElement(locator).sendKeys(text);
+            String existingText = driver.findElement(locator).getAttribute("value");
+            if (! text.equals(existingText)) {
+                driver.findElement(locator).clear();
+                driver.findElement(locator).sendKeys(text);
+            }
         }
     }
 
